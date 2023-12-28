fc188476bf5490ee1b2e64305e90c0eec22462b8
SurryChen <1974023122@qq.com>
2023-12-28 13:28:49 +0800

    [test] test nat

--- src/main/java/com/somecode/colorfulsync/Main.java
+++ src/main/java/com/somecode/colorfulsync/Main.java
@@ -1,132 +1,218 @@
 package com.somecode.colorfulsync;
 
+import com.alibaba.fastjson2.JSON;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("server start");
+    /**
+     * 所有连接IP - 键值对是（设备ID，节点信息）
+     */
+    static Map<String, Node> deviceConnect = new HashMap<>();
 
+    /**
+     * 套接字连接
+     */
+    static DatagramSocket server;
+
+    /**
+     * 端口
+     */
+    static int PORT = 8002;
+
+    public static void main(String[] args) throws IOException {
         // 绑定端口
-        DatagramSocket server = new DatagramSocket(8002);
+        server = new DatagramSocket(PORT);
         // 缓冲区
         byte[] buf = new byte[1024];
-        // 数据包
+        // 给UDP接收包设置缓冲区
         DatagramPacket packet = new DatagramPacket(buf, buf.length);
-        // 节点数量
-        int count = 0;
-        // 数量的字符串形式
-        String count_S;
-        // 节点A和节点B的出口IP
-        String clientA_IP = "", clientB_IP = "";
-        // 节点A和节点B的IP:post拼接的字符串
-        String ClientA = "", ClientB = "";
-        // 节点A和节点B的出口端口
-        int clientA_port = 0, clientB_port = 0;
-        //
-        InetAddress clientA_address = null, clientB_address = null;
         // 循环监听
         while (true) {
             // 监听数据，并放入到packet中
             server.receive(packet);
-            // 拿出数据
+            // 一个UDP包最大是64k，这里应该还不会变得太大
             String requestMessage = new String(packet.getData(), 0, packet.getLength());
-            // 输出数据
-            System.out.println(requestMessage);
-            // requestMessage类似于指令
-            if (requestMessage.contains("Request ID")) {
-                // 客户端+1
-                count++;
-                // 字符串形式
-                count_S = String.valueOf(count);
-                // 判断id的单双
-                if (count % 2 != 0) {
-                    // 单数填充A
-                    clientA_port = packet.getPort();
-                    clientA_address = packet.getAddress();
-                    ClientA = count_S + ":" + clientA_address.getHostAddress() + ":" + clientA_port;
-                    // 输出A的信息
-                    System.out.println("client " + ClientA);
-                    clientA_IP = clientA_address.getHostAddress() + ":" + clientA_port;
-                    // 分配ID
-                    sendID(count_S, clientA_port, clientA_address, server);
-                } else {
-                    // 偶数填充B
-                    clientB_port = packet.getPort();
-                    clientB_address = packet.getAddress();
-                    ClientB = count_S + ":" + clientB_address.getHostAddress() + ":" + clientB_port;
-                    System.out.println("client " + ClientB);
-                    clientB_IP = clientB_address.getHostAddress() + ":" + clientB_port;
-                    // 分配ID
-                    sendID(count_S, clientB_port, clientB_address, server);
-                    // 延时预防丢包问题
-                    try {
-                        // 延时1秒
-                        Thread.sleep(1000);
-                    } catch (Exception e) {
-                        System.out.println("Got an exception!");
-                    }
-                    // 异步给新节点与当前已读节点发送对方的的节点信息
-                    sendIP(ClientA, clientB_port, clientB_address, server);
-                    sendIP(ClientB, clientA_port, clientA_address, server);
-                }
+            // 打印
+            System.out.println("收到数据包：" + requestMessage);
+            // 反序列化指令
+            Command command = JSON.parseObject(requestMessage, Command.class);
+            // 获取IP和端口
+            InetAddress address = packet.getAddress();
+            int port = packet.getPort();
+            // 判断
+            if (command.type == 1) {
+                heartbeatProcessor(command, address, port);
+            } else if (command.type == 2) {
+                requestTargetDeviceProcessor(command, address, port);
+            } else {
+                continue;
             }
         }
     }
 
-    private static void sendID(String id, int port, InetAddress address, DatagramSocket server) {// 发送ID
-        // 返回id
-        byte[] sendBuf = id.getBytes();
-        // 创建一个upd包，制定了大小、ip、端口
-        DatagramPacket sendPacket = new DatagramPacket(sendBuf, sendBuf.length, address, port);
-        // 发送回去
+    /**
+     * 心跳指令处理器
+     * @param command
+     * @param address
+     * @param port
+     */
+    private static void heartbeatProcessor(Command command, InetAddress address, int port) {
+        // 是否有连接
+        if (deviceConnect.get(command.sourceDeviceID) == null) {
+            // 创建一个Node节点
+            Node node = new Node();
+            node.ip = address.getHostAddress();
+            node.port = port;
+            node.lastHeartBeatTimestamp = new Date().getTime();
+            node.deviceID = command.sourceDeviceID;
+            deviceConnect.put(command.sourceDeviceID, node);
+        } else {
+            // 更新最后一次心跳时间
+            Node node = deviceConnect.get(command.sourceDeviceID);
+            node.lastHeartBeatTimestamp = new Date().getTime();
+        }
+    }
+
+    /**
+     * 请求目标IP处理器
+     * @param command
+     * @param address
+     * @param port
+     */
+    private static void requestTargetDeviceProcessor(Command command, InetAddress address, int port) {
+        // 返回节点信息
+        Node node = null;
+        // 查看缓存
+        if (deviceConnect.get(command.targetDeviceID) != null) {
+            // 创建一个空的告诉对方没有或者连接超时
+            Node findNode = deviceConnect.get(command.targetDeviceID);
+            long nowTime = new Date().getTime();
+            if (nowTime - findNode.lastHeartBeatTimestamp < 3000) {
+                node = findNode;
+            }
+        }
+        // 如果没有对应的信息，创建一个空的进行返回
+        if (node == null) {
+            node = new Node();
+        }
+        String returnMessage = JSON.toJSONString(node);
+        System.out.println("返回的信息：" + returnMessage);
+        byte[] bytes = returnMessage.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(bytes, bytes.length, address, port);
         try {
             server.send(sendPacket);
-            System.out.println("ID assignment successful!");
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void sendIP(String Client, int port, InetAddress address, DatagramSocket server) {// 发送节点信息
-        // 返回ip和端口
-        byte[] sendBuf = Client.getBytes();
-        DatagramPacket sendPacket = new DatagramPacket(sendBuf, sendBuf.length, address, port);
-        try {
-            server.send(sendPacket);
-        } catch (IOException e) {
-            e.printStackTrace();
+    /**
+     * 节点信息
+     */
+    static class Node {
+        /**
+         * 设备ID
+         */
+        String deviceID;
+
+        /**
+         * 节点IP
+         */
+        String ip;
+
+        /**
+         * 节点端口
+         */
+        int port;
+
+        /**
+         * 最后一次心跳的时间戳
+         */
+        long lastHeartBeatTimestamp;
+
+        public void setDeviceID(String deviceID) {
+            this.deviceID = deviceID;
+        }
+
+        public void setIp(String ip) {
+            this.ip = ip;
+        }
+
+        public void setPort(int port) {
+            this.port = port;
+        }
+
+        public void setLastHeartBeatTimestamp(long lastHeartBeatTimestamp) {
+            this.lastHeartBeatTimestamp = lastHeartBeatTimestamp;
+        }
+
+        public String getDeviceID() {
+            return deviceID;
+        }
+
+        public String getIp() {
+            return ip;
+        }
+
+        public int getPort() {
+            return port;
+        }
+
+        public long getLastHeartBeatTimestamp() {
+            return lastHeartBeatTimestamp;
         }
     }
 
-//    private static ArrayList<String> c = new ArrayList<String>();
-//    public static void main(String[] args) throws IOException, InterruptedException {
-//        DatagramSocket server = new DatagramSocket(8002);
-//        byte[] bytes = new byte[1024];
-//        while (true) {
-//            DatagramPacket packet = new DatagramPacket(bytes, bytes.length);
-//            server.receive(packet);
-//            InetAddress address = packet.getAddress();
-//            int port = packet.getPort();
-//            String s = address.getHostAddress()+":"+port;
-//            System.out.println("收到->"+s+"的消息：");
-//
-//            if (!c.contains(s)){
-//                c.add(s);
-//            }
-//
-//            for (String s1 : c) {
-//                if (!s1.equals(s)){
-//                    byte[] b = ("this is server:"+s1).getBytes();
-//                    DatagramPacket packet1 = new DatagramPacket(b, b.length, address,port);
-//                    server.send(packet1);
-//                }
-//            }
-//        }
-//    }
+    /**
+     * 指令信息
+     */
+    static class Command {
+        /**
+         * 指令类型 - 1：心跳；2：请求指定设备的IP
+         */
+        int type;
+
+        /**
+         * 源设备的ID
+         */
+        String sourceDeviceID;
+
+        /**
+         * 目标设备的ID
+         */
+        String targetDeviceID;
+
+        public void setType(int type) {
+            this.type = type;
+        }
+
+        public void setSourceDeviceID(String sourceDeviceID) {
+            this.sourceDeviceID = sourceDeviceID;
+        }
+
+        public void setTargetDeviceID(String targetDeviceID) {
+            this.targetDeviceID = targetDeviceID;
+        }
+
+        public int getType() {
+            return type;
+        }
+
+        public String getSourceDeviceID() {
+            return sourceDeviceID;
+        }
+
+        public String getTargetDeviceID() {
+            return targetDeviceID;
+        }
+    }
 
 }
